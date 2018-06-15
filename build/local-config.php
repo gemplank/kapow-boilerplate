a561d10f84e21da84e0914b539179e119cd94c76
Charlie Tapster <charlie@makedo.net>
2018-06-15 13:29:13 +0100

    configs

---
+++ build/local-config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Local Config.
+ *
+ * @package My Project
+ */
+
+define( 'DB_NAME', 'kapow-wp-db' );
+define( 'DB_USER', 'root' );
+define( 'DB_PASSWORD', '' );
+define( 'DB_HOST', '127.0.0.1' );
+define( 'DB_CHARSET', 'utf8' );
+define( 'WP_DEBUG', 'true' );
