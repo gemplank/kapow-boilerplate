ea9c3f2e009b9e81203eb5e2053001b1df3412c6
Charlie <charlie@makedo.net>
2018-08-06 10:02:54 +0100

    example elements

---
+++ kapow-boilerplate/build/wp-content/themes/my-project/template-parts/static/modules/hero-internal-narrow.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Template part for displaying a message that posts cannot be found.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package My Project
+ *
+ *
+ */
+?>
+
+<header class="hero-internal hero-internal--narrow" style="background-image: url('https://source.unsplash.com/OL5aDIJCysM/1600x900')">
+	<div class="hero-internal__overlay"></div>
+	<div class="hero-internal__wrap">
+		<div class="hero-internal__content">
+			<h1 class="hero-internal__heading sr-fade">Hero Internal Narrow</h1>
+			<div class="hero-internal__text sr-fade">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id optio, rerum quaerat, repellendus praesentium nulla voluptas.</p>
+			</div>
+		</div>
+	</div>
+</header>
---
+++ kapow-boilerplate/build/wp-content/themes/my-project/template-parts/static/structures/test-structure.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @flags wrap dark
+ */
+ ?>
+
+<ul>
+	<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut nemo consequuntur molestiae eum dignissimos, iusto magnam ad maiores harum voluptate possimus ipsa illo id at consectetur? Deleniti dolore, perspiciatis sequi.</li>
+	<li>Eligendi ipsa dolor consequuntur officia similique eos voluptas, beatae eum recusandae labore, error vitae iusto tenetur accusantium hic eius assumenda maxime ipsam. Harum ducimus qui nulla quis porro mollitia officia.</li>
+	<li>Libero, illo aliquid optio ex neque eius quam perferendis, voluptatibus impedit, praesentium inventore. Accusantium ipsa vero, quae sapiente commodi repellat, sed labore ex doloribus consectetur voluptatum! Sint explicabo inventore asperiores!</li>
+</ul>
